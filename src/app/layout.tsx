c5c6c6d07a85983e6c7b201fcf23d78b5f9bf453
Роман Бубнов <kilkun@icloud.com>
2025-09-03 20:01:13 +0400

    feat: reset readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import '@/styles/globals.css';
-import { ThemeProvider } from '@/theme/ThemeProvider'; // <- импорт провайдера
+import { ThemeProvider } from '@/theme/ThemeProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
