61e84fa91f0749e713cc425601b05f7eba0a5d73
Роман Бубнов <kilkun@icloud.com>
2025-09-08 11:46:08 +0400

    feat: update client page

---
+++ src/app/[locale]/[method]/[[...params]]/page.tsx
@@ -0,0 +1,18 @@
+import { useAtomValue } from 'jotai';
+import { redirect } from 'next/navigation';
+
+import RestClientWrapper from '@/components/client/RestClientWrapper';
+import { authAtom } from '@/store/apiAtom';
+import { ReadonlyFC } from '@/types';
+
+const ClientPageWrapper: ReadonlyFC = () => {
+  const user = useAtomValue(authAtom);
+
+  if (!user) {
+    redirect('/login');
+  }
+
+  return <RestClientWrapper />;
+};
+
+export default ClientPageWrapper;
