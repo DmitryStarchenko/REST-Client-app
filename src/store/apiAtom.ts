67402acc6321c248e20ea948755f34b5583efea2
Роман Бубнов <kilkun@icloud.com>
2025-09-09 09:07:29 +0400

    chore: update indexes

--- src/store/apiAtom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'jotai';
-
-export const requestAtom = atom({
-  url: '',
-  method: 'GET',
-  headers: {},
-  body: null,
-});
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,1 @@
-export * from './apiAtom';
 export * from './authAtom';
