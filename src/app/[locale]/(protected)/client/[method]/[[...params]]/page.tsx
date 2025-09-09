96026977388061cf27d89dec2d48c069f04af2ce
Роман Бубнов <kilkun@icloud.com>
2025-09-09 09:31:43 +0400

    fix: add a separate route for client page

---
+++ src/app/[locale]/(protected)/client/[method]/[[...params]]/page.tsx
@@ -0,0 +1,8 @@
+import RestClientWrapper from '@/components/client/RestClientWrapper';
+import { ReadonlyFC } from '@/types';
+
+const ClientPageWrapper: ReadonlyFC = async () => {
+  return <RestClientWrapper />;
+};
+
+export default ClientPageWrapper;
