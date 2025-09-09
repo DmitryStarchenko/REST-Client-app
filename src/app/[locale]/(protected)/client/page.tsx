d9e7fa11d78a6893d3a985f77392e2710541b8fb
Роман Бубнов <kilkun@icloud.com>
2025-09-09 09:31:43 +0400

    fix: add a separate route for client page

--- src/app/[locale]/(protected)/client/page.tsx
+++ src/app/[locale]/(protected)/client/page.tsx
@@ -1,14 +1,7 @@
-'use client';
+import { FC } from 'react';
 
-import dynamic from 'next/dynamic';
+const Client: FC = () => {
+  return <div>Client</div>;
+};
 
-import { ReadonlyFC } from '@/types';
-
-const RestClientWrapper = dynamic(() => import('@/components/client'), {
-  ssr: false,
-  loading: () => <div>Loading REST client...</div>,
-});
-
-const ClientIndex: ReadonlyFC = () => <RestClientWrapper />;
-
-export default ClientIndex;
+export default Client;
