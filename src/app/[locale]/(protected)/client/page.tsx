8ac3df4542c39d154cb1da19e1af3326b60c7ff6
Роман Бубнов <kilkun@icloud.com>
2025-09-12 16:27:33 +0400

    fix: update rest client component routing

--- src/app/[locale]/(protected)/client/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-import { ReadonlyFC } from '@/types';
-
-const ClientIndexPage = (): ReadonlyFC => {
-  redirect('./client/GET');
-};
-
-export default ClientIndexPage;
