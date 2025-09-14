7fee7297e835c55d108b448335139e356ceea075
Роман Бубнов <kilkun@icloud.com>
2025-09-14 16:40:16 +0400

    feat: update routing ligic for client page

---
+++ src/app/[locale]/(protected)/client/[method]/[[...params]]/page.tsx
@@ -0,0 +1,17 @@
+import { notFound } from 'next/navigation';
+
+import ClientWrapper from '@/components/client';
+import { ReadonlyFC, PageProps } from '@/types';
+import { isValidMethod } from '@/utils';
+
+const MethodPage: ReadonlyFC<PageProps> = async ({ params }) => {
+  const { method } = await params;
+  const methodUpper = (method ?? '').toUpperCase();
+
+  if (!isValidMethod(methodUpper)) {
+    notFound();
+  }
+  return <ClientWrapper />;
+};
+
+export default MethodPage;
