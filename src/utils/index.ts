d4d231a21166ef57600bdc26a8f0ef5465957bf5
Роман Бубнов <kilkun@icloud.com>
2025-09-17 08:16:15 +0400

    chore: update indexes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,4 @@ export * from './get-query-client';
 export * from './uid';
 export * from './generateSnippet';
 export * from './isJson';
-export * from './isValidMethod';
+export * from './isSuccess';
