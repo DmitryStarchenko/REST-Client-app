baf5980dbc3f43bb653c5ad992dbd3c7cd07539e
Dmitry <Dololob92@gmail.com>
2025-09-18 23:02:25 +0300

    feat: added history page

--- src/app/[locale]/(protected)/history/page.tsx
+++ src/app/[locale]/(protected)/history/page.tsx
@@ -1,7 +1,14 @@
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
+
+import LayoutHistory from '@/components/layout/History/History';
+import Loader from '@/components/shared/Loader/Loader';
 
 const History: FC = () => {
-  return <div>History</div>;
+  return (
+    <Suspense fallback={<Loader />}>
+      <LayoutHistory />
+    </Suspense>
+  );
 };
 
 export default History;
