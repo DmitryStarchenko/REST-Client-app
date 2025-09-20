7a2951796479055cc032fe2b26a8b835f1e62c79
Dmitry <Dololob92@gmail.com>
2025-09-21 00:02:35 +0300

    feat: add tests for layout, notfound and page in app

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,8 @@
 'use client';
+import React from 'react';
 
 import ErrorPage from '@/components/shared/ErrorPage/ErrorPage';
 import { ReadonlyFC } from '@/types/readonly.types';
-
-// This page renders when a route like `/unknown.txt` is requested.
-// In this case, the layout at `app/[locale]/layout.tsx` receives
-// an invalid value as the `[locale]` param and calls `notFound()`.
 
 const GlobalNotFound: ReadonlyFC = () => {
   return (
