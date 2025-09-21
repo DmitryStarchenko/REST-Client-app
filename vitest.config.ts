baf9a2d757cc203b8fe3b9ba1a47b0077ef35386
Dmitry <Dololob92@gmail.com>
2025-09-22 02:59:23 +0300

    fix: errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,20 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
+    coverage: {
+      enabled: true,
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/types/**',
+        'commitlint.config.js',
+        'eslint.config.mj',
+        'next.config.ts',
+        'vitest.config.ts',
+        // Исключает папку 'types' на любом уровне
+      ],
+      // Другие настройки покрытия, если они есть
+    },
   },
   resolve: {
     alias: {
